d9ca42124b106c48ffdbf2c17faacd0f40fb57e5
Iflaq khurshid <112414810+Iflaqbhat@users.noreply.github.com>
2024-10-27 12:26:35 +0530

    created a submission

---
+++ script.js
@@ -0,0 +1,11 @@
+document.body.innerHTML = '';
+
+        // Listen for the DOMContentLoaded event
+        document.addEventListener('DOMContentLoaded', function() {
+            // Create a new paragraph element
+            const paragraph = document.createElement("h3");
+            // Set the text content of the paragraph
+            paragraph.textContent = "DOM load success";
+            // Append the paragraph to the body
+            document.body.appendChild(paragraph);
+        });
