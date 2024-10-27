30279e5631276d74a73f36c640409936b0f8ae97
Iflaq khurshid <112414810+Iflaqbhat@users.noreply.github.com>
2024-10-27 12:19:43 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,3 +1,8 @@
- window.onload = function() {
-	 document.write("<p>DOM load success</p>");
- };
+ document.addEventListener('DOMContentLoaded', function() {
+            // Create a new paragraph element
+            const paragraph = document.createElement("p");
+            // Set the text content of the paragraph
+            paragraph.textContent = "DOM load success";
+            // Append the paragraph to the body
+            document.body.appendChild(paragraph);
+        });
